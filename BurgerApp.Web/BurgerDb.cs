c936df67ba5fa2eecd3e482b6ab0ce5376f8a187
Александар Дојчиновски <Dojchinovski@live.com>
2022-06-20 13:55:09 +0200

    add mvc homework class02

--- BurgerApp.Web/BurgerDb.cs
+++ BurgerApp.Web/BurgerDb.cs
@@ -1,7 +1,13 @@
-﻿namespace BurgerApp.Web
+﻿using BurgerApp.Web.Models;
+
+namespace BurgerApp.Web
 {
     public static class BurgerDb
     {
+        public static List<Burger> Burgers = new();
 
+        public static List<Order> Orders = new();
+
+        
     }
 }
